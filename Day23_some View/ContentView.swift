98c88af3e39cbeb64e481f8acae7b2faabd56f82
Elias Breitenbach <elias.b@me.com>
2023-03-15 21:32:50 +0100

    Pushed the project to github

---
+++ Day23_some View/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Day23_some View
+//
+//  Created by Elias Breitenbach on 08.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Text("World")
+            Text("Goodbye")
+            Text("World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day23_some View/Day23_some_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day23_some_ViewApp.swift
+//  Day23_some View
+//
+//  Created by Elias Breitenbach on 08.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Day23_some_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
